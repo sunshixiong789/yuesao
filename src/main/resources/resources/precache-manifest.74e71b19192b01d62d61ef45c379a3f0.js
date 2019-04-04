8220cbab6bd116164c7e284c51ad178c97c8fecc
sunshixiong <1285913468@qq.com>
2019-04-04 18:00:32 +0800

    添加静态资源

---
+++ src/main/resources/resources/precache-manifest.74e71b19192b01d62d61ef45c379a3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7d787e8364ffc40dcfd",
+    "url": "/static/css/main.9944f889.chunk.css"
+  },
+  {
+    "revision": "d7d787e8364ffc40dcfd",
+    "url": "/static/js/main.6feacfb8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e1b52afa8645fc5644f6",
+    "url": "/static/css/2.53c4d537.chunk.css"
+  },
+  {
+    "revision": "e1b52afa8645fc5644f6",
+    "url": "/static/js/2.135eed5d.chunk.js"
+  },
+  {
+    "revision": "d7bd44ff100c41080e7db68e98e8a052",
+    "url": "/index.html"
+  }
+];
